f9bb2cb7502f5c00977419a806797e12fd0585ae
shabbir-careem <shabbir.akolawala@careem.com>
2021-11-07 17:13:47 +0400

    Update the config to load .env file from root

--- config/__init__.py
+++
@@ -1,5 +0,0 @@
-import inspect
-
-stack = inspect.stack()
-calling_context = next(context for context in stack if context.filename != __file__)
-print(calling_context.filename)
--- config/config.py
+++ config/config.py
@@ -1,1 +1,13 @@
+import inspect
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
 # Configuration setting for the bot
+
+def loadConf():
+    stack = inspect.stack()
+    calling_context = next(context for context in stack if context.filename != __file__)
+    root_dir = os.path.dirname(calling_context.filename)
+    env_path = Path(root_dir) / '.env'
+    load_dotenv(dotenv_path=env_path)
